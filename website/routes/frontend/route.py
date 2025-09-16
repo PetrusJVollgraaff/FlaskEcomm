f6634de8b9089f1a9fca07f9194b5fa8c678f742
PetrusJVollgraaff <petrusvollgraaff1988@hotmail.co.za>
2025-09-16 15:36:07 +0200

    add fontend pages

--- website/routes/frontend/route.py
+++ website/routes/frontend/route.py
@@ -1,7 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
 
 views = Blueprint('views', __name__, template_folder="templates")
 
 @views.route('/')
 def index():
     return  render_template("home.html")
+
+@views.route('/pages/products')
+def products():
+    return  render_template("products.html")
+
+@views.route('/pages/products/<int:product_id>')
+def product(product_id):
+    print(product_id)
+    if product_id:
+        return  render_template("product.html")
+    return  redirect(url_for('views.index'))
