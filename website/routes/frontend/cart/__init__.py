b10d1f4e88aa6eb87cc8909ba7f9a59807014436
PetrusJVollgraaff <petrusvollgraaff1988@hotmail.co.za>
2025-09-30 11:34:23 +0200

    update cart

--- website/routes/frontend/cart/__init__.py
+++ website/routes/frontend/cart/__init__.py
@@ -1,5 +1,6 @@
 from website.app import db
+from sqlalchemy import text
 from flask import Blueprint, jsonify, render_template, session, request
 
 
-cartpages = Blueprint('cartpages', __name__, template_folder="templates", url_prefix="/cart")
+cartpages = Blueprint('cartpages', __name__, template_folder="templates", url_prefix="/cart", static_folder='static',)
--- website/routes/frontend/cart/route.py
+++ website/routes/frontend/cart/route.py
@@ -1,19 +1,65 @@
 from . import *
 
+sqlproduct = '''
+    SELECT 
+        P.id, 
+        P.name, 
+        P.description,
+        P.code,
+        P.instock,
+        P.onspecial,
+        P.showonline,
+
+        --product main image
+        IFNULL(M.id, 0) AS imgid,
+        IFNULL(MU.id, 0) AS imgusedid,
+        CASE 
+            WHEN M.id IS NOT NULL THEN
+                '/static/images/'|| M.name ||'/medium'|| M.ext
+            ELSE ''
+        END AS imgpath,
+        
+        --product normal prices
+        IFNULL(PP1.id, 0) AS normalpriceid,
+        IFNULL(PP1.price, 0.00) as normalprice,
+
+        --product special prices
+        IFNULL(PP2.id, 0) AS specialpriceid,
+        IFNULL(PP2.price, 0.00) as specialprice,
+        PP2.specialdateStart,
+        PP2.specialdateEnd
+
+    FROM products AS P
+    LEFT JOIN mediaused MU ON MU.id = P.mediaused_id AND MU.deleted_yn=0
+    LEFT JOIN medias M ON M.id = MU.media_id AND M.deleted_yn=0
+    LEFT JOIN productprice PP1 ON PP1.product_id = P.id AND PP1.isspecial=0 AND PP1.deleted_yn = 0
+    LEFT JOIN productprice PP2 ON PP2.product_id = P.id AND PP2.isspecial=1 AND PP2.deleted_yn = 0
+    WHERE P.deleted_yn = 0 AND P.showonline = 1
+'''
+
+
 def get_cart():
     return session.get('cart', {})
 
 @cartpages.route("/")
 def cart():
     cart = get_cart()
-    print(cart)
-    items, toatal=[], 0.0
 
-    for pid, qty in cart.items():
-        pass
-    pass
+    productsArr = [pid for pid, qty in cart.items()]
+    productIds = ",".join(productsArr)
+    
+    sql = text(f'{sqlproduct}AND P.id IN ({productIds})')
+    result= db.session.execute(sql)
+    products = [dict(row) for row in result.mappings()]
 
-    return  render_template("cart.html")
+    for product in products:
+        id = str(product["id"])
+        qty = cart[id]
+        product["qty"] = qty
+
+    total = sum( [products["qty"] * products["normalprice"]  for products in  products] )
+    
+    return  render_template("cart.html", products=products, total=total)
 
 @cartpages.route("/add/", methods=["POST"])
 def cart_add():    
